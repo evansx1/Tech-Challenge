aacf87f1089c5ba9e42d020b74ea4f6a64b2032e
evansx1 <douglasleal.15@gmail.com>
2018-10-15 11:57:03 -0300

    mycommit

---
+++ DouglasLeal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Douglas_Leal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
